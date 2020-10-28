85cf452b53837cbdc13d31559414588cf008b0ac
cumalitezcan <cumalitezcan@gmail.com>
2020-10-28 12:20:03 +0300

    event

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react'
 import Navi from './Navi';
 import CategoryList from './CategoryList';
 import ProductList from './ProductList';
 import { Container, Row, Col } from 'reactstrap';
 
-function App() {
-  let categoryInfo = {title: "Category List"}
-  let productInfo = {title: "Product List"}
+export default class App extends Component {
 
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-         <Col xs="3"> <CategoryList info={categoryInfo} /> </Col>
-         <Col cs="9"> <ProductList info={productInfo} /> </Col>
-        </Row>
-      </Container>
+  state = {currentCategory:""}
 
-      
-    </div>
-  );
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  }
+
+  render() {
+    let categoryInfo = { title: "Category List" }
+    let productInfo = { title: "Product List" }
+
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3"> <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} /> </Col>
+            <Col cs="9"> <ProductList currentCategory={this.state.currentCategory} info={productInfo} /> </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+
+
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -8,16 +8,22 @@ export default class CategoryList extends Component {
             { categoryId: 2, categoryName: "Condiments" }
         ]
     };
+
+    
+
     render() {
         return (
             <div>
                 <h2>{this.props.info.title}</h2>
                 <ListGroup>
                     {this.state.categories.map(category => (
-                        <ListGroupItem key={category.categoryId}> {category.categoryName} </ListGroupItem>
+                        <ListGroupItem 
+                        onClick = {() => {this.props.changeCategory(category)}}
+                        key={category.categoryId}> {category.categoryName} </ListGroupItem>
                     ) )
                     }
                 </ListGroup>
+                <h2>{this.props.currentCategory}</h2>
             </div>
         )
     }
